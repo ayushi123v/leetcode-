f9cf0a6ac1f16d0fd4430a658190ff4e03ce846a
Ayushi Varshney <151488662+ayushi123v@users.noreply.github.com>
2026-06-28 11:18:50 +0530

    Time: 1 ms (31.16%), Space: 42.4 MB (46.81%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0 ;
+        String binary = Integer.toBinaryString(n);
+        for(int i = 0 ; i<binary.length() ; i++){
+            if (binary.charAt(i) =='1'){
+                count ++;
+            }
+        }
+        return count;
+
+    }
+}
